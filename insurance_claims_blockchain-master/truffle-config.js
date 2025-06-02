98529d13fad436da2723f283be392a499a2affba
zinebdri <zinebdrissi2004@gmail.com>
2025-06-02 15:02:37 +0100

    Update and rename truffle.js to truffle-config.js

---
+++ insurance_claims_blockchain-master/truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // match any network id
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.4.24",
+    },
+  },
+};
